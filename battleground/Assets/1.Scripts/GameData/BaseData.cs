0e7b16cd2b6815d1234962130eab66d82cbeaf26
onasheep <inabass@naver.com>
2023-03-13 22:52:32 +0900

    Feat : add SoundClip and SoundData
    set SoundClip and SoundData to make Sound Manager.
    fixed error.

---
+++ battleground/Assets/1.Scripts/GameData/BaseData.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  data의 기본 클래스입니다.
+///  공통적인 데이터를 가지고 있게 되는데 , 이름만 현재 가지고 있다.
+///  데이터의 갯수와 이름의 목록 리스트를 얻을 수 있다.
+/// </summary>
+public class BaseData : ScriptableObject
+{
+    public const string dataDirectory = "/9.ResourcesData/Resources/Data/";
+    public string[] names = null;
+
+
+    public BaseData() { }
+
+    public int GetDataCount()
+    {
+        int retValue = 0;
+
+        if(this.names != null)
+        {
+            retValue = this.names.Length;
+        }
+
+        return retValue;
+    }
+
+    /// <summary>
+    /// 툴에 출력하기 위한 이름 목록을 만들어 주는 함수
+    /// </summary>
+    /// <param name=""></param>
+    /// <returns></returns>
+    public string[] GetNameList(bool showID, string filtrerWord = "")
+    {
+        string[] retList = new string[0];
+        if(this.name == null)
+        {
+            return retList;
+        }
+        retList = new string[this.names.Length];
+
+        for (int i = 0; i < this.name.Length; i++)
+        {
+            if(filtrerWord != "")
+            {
+                if (names[i].ToLower().Contains(filtrerWord.ToLower()) == false)
+                {
+                    continue;
+                }
+            }
+            if(showID)
+            {
+                retList[i] = i.ToString() + " : " + this.names[i];
+            }
+            else
+            {
+                retList[i] = this.names[i];
+            }
+        }
+
+        return retList;
+    }
+
+    public virtual int AddData(string newName)
+    {
+        return GetDataCount();
+    }
+
+    public virtual void RemoveData(int index)
+    {
+
+    }
+    public virtual void Copy(int index)
+    {
+
+    }
+
+
+}
